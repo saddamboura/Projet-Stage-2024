782af9508390b8958337e0778142dd36e07b62b6
SADAM Ibrahim <saddamboura4400@gmail.com>
2024-12-17 10:44:00 +0100

    Premier programme de notre projet de stage

---
+++ projet.js
@@ -0,0 +1,30 @@
+// Sélectionne l'élément par son ID
+document.getElementById("menu-icon").addEventListener("click", function () {
+  // Redirige vers la page projetInscri.html
+  window.location.href = "./projetAuth.html";
+});
+// Sélectionne l'élément par son ID
+document.getElementById("menu-icon1").addEventListener("click", function () {
+  // Redirige vers la page projetInscri.html
+  window.location.href = "./projetAuth.html";
+});
+
+// bouton Acceuil pour commencer un cours
+document.getElementById("btn-Acceuil").addEventListener("click", function () {
+  // Redirige vers la page projetInscri.html
+  window.location.href = "./categorieA.html";
+});
+
+// +++++++++++++++++LIENS++++++++++++++++++++++++++++
+// SélectionneR LE BOUTON A
+document.getElementsByClassName("catA").addEventListener("click", function () {
+  // Redirige vers la page projetInscri.html
+  window.location.href = "./categorieA.html";
+});
+// SélectionneR LE BOUTON B
+document.getElementsByClassName("catB").addEventListener("click", function () {
+  // Redirige vers la page projetInscri.html
+  window.location.href = "./categorieB.html";
+});
+
+// =======================Création des pages des sous-menus ===============
---
+++ projetInscri.js
@@ -0,0 +1,5 @@
+// Sélectionne l'élément par son ID
+document.getElementById("IdiconAcceuil").addEventListener("click", function () {
+  // Redirige vers la page projetInscri.html
+  window.location.href = "./projet.html";
+});
